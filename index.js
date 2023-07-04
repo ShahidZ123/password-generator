bf0412fd38995b1439d64b4e311804e38cd8161e
ShahidZ123 <farhankhan745821@gmail.com>
2023-07-04 11:41:10 +0530

    final java cript addition

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
 let password = "";
 let passwordLength = 10;
-let checkCount = 1;
+let checkCount = 0;
 
 handleSlider()
 //set strength circle to grey
@@ -37,7 +37,7 @@ function setIndicator(color) {
 }
 
 function getRandomInteger(min, max) {
-     Math.floor(Math.random() * (max-min)) + min;
+    return Math.floor(Math.random() * (max-min)) + min;
 }
 
 
@@ -69,15 +69,148 @@ function calcStrength() {
     if(numbersCheck.checked) hasNum = true;
     if(symbolsCheck.checked) hasSym = true;
 
-    if(hasUpper && hasLower && (hasNum || hasSym) >=8) {
+    if(hasUpper && hasLower && (hasNum || hasSym) && passwordLength >=8) {
         setIndicator("#0f0")
     } else if(
         (hasLower || hasUpper) &&
         (hasNum || hasSym) &&
         passwordLength >= 6
     ){
-        setIndicator("ff0");
+        setIndicator("#ff0");
     } else {
         setIndicator("#f00");
     }
 }
+
+async function copyContent() {
+    try {
+        await  navigator.clipboard.writeText(passwordDisplay.value);
+        copyMsg.innerText = "Copied";
+    }
+    catch(e) {
+        copyMsg.innerText = "Failed!";
+    }
+    //to make copy wala span visible
+    copyMsg.classList.add("active");
+
+    setTimeout(() => {
+        copyMsg.classList.remove("active")
+    }, 2000);
+}
+
+    function shufflePassword(array) {
+    //Fisher Yates Method
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    let str = "";
+    array.forEach((el) => (str += el));
+    return str;
+}
+
+
+
+function handleCheckBoxChange() {
+    checkCount = 0;
+    allCheckBox.forEach((checkbox) => {
+        if(checkbox.checked)
+            checkCount++;
+    });
+    
+    //special condition
+    if(passwordLength < checkCount) {
+        passwordLength = checkCount;
+        handleSlider();
+    }
+}
+
+allCheckBox.forEach((checkbox) => {
+    checkbox.addEventListener('change', handleCheckBoxChange);
+})
+
+inputSlider.addEventListener('input', (e) => {
+    passwordLength = e.target.value;
+    handleSlider();
+})
+
+copyBtn.addEventListener('click', () => {
+    if(passwordDisplay.value)
+        copyContent();
+})
+
+
+
+generateBtn.addEventListener('click', () => {
+    //none of the checkbox are selected
+    if(checkCount<=0) return;
+
+    if(passwordLength < checkCount) {
+        passwordLength = checkCount;
+        handleSlider();
+    }
+
+    //let's start the journey to find new password
+
+    //remove old password
+    password = "";
+
+    //let's put the stuff mentioned by checkboxes
+    // if(uppercaseCheck.checked) {
+    //     password += generateUpperCase();
+    // }
+
+    // if(lowercaseCheck.checked) {
+    //     password += generateLowerCase();
+    // }
+
+    // if(numbersCheck.checked) {
+    //     password += generateRandomNumber();
+    // }
+
+    // if(symbolsCheck.checked) {
+    //     password += generateSymbol();
+    // }
+
+    let funcArr = [];
+
+    if(uppercaseCheck.checked) 
+      funcArr.push(generateUpperCase);
+    
+    if(lowercaseCheck.checked) 
+        funcArr.push(generateLowerCase);
+    
+    if(numbersCheck.checked)
+        funcArr.push(generateRandomNumber);
+    
+    if(symbolsCheck.checked)
+        funcArr.push(generateSymbol);
+
+    //compulsary addition
+    for(let i=0; i<funcArr.length; i++) {
+        password += funcArr[i]();
+    }
+    console.log("COmpulsory adddition done");
+
+    //remaining addition
+    for(let i =0; i<passwordLength-funcArr.length; i++) {
+        let randIndex = getRandomInteger(0, funcArr.length);
+        console.log("randIndex " + randIndex);
+        password += funcArr[randIndex]();
+    }
+    console.log("Remaining adddition done");
+
+    //shuffle the password
+    password = shufflePassword(Array.from(password));
+    console.log("Shuffling done");
+    //show in UI
+    passwordDisplay.value = password;
+    console.log("UI adddition done");
+
+    //calculate strength
+    calcStrength();
+});
+
+//turezza technology
